818c062c719fd479cbb45a0656ba05c6367daebe
Ashraful Islam <rx.ju.edu@gmail.com>
2024-06-27 22:16:17 +0700

    Morgan for logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -12,11 +12,14 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("tiny"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../", "public")));
 
 app.use(planetsRouter);
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "..", "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
